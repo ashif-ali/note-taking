8fd42f521fef3580719a4c281e942e81e9ca3d1c
Sk Md Ashif Ali <36637666+ashif-ali@users.noreply.github.com>
2025-08-13 18:50:18 +0530

    setup vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Load environment variables from .env files
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    // Use the variable from the 'env' object
+    base: env.VITE_BASE_PATH || "/note-taking"
+  }
 })
